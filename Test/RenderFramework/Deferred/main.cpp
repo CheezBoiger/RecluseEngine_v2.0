64e17d86837b27a6b9e2150ca3ab4d5a32fc09cd
Mario Garcia <alexander.garcia18@yahoo.com>
2026-03-25 02:44:38 -0700

    Add config to disable vulkan dll if no vulkan is added to build.

--- Test/RenderFramework/Deferred/main.cpp
+++ Test/RenderFramework/Deferred/main.cpp
@@ -747,7 +747,7 @@ int main(char* argv[], int c)
     LogSystem::initializeLoggingSystem();
     LogSystem::enableLogTypes(LogType_Debug | LogType_Info);
     RealtimeTick::initializeWatch(1ull, 0);
-    instance  = GraphicsInstance::create(GraphicsApi_Vulkan);
+    instance  = GraphicsInstance::create(GraphicsApi_Direct3D12);
     GraphicsAdapter* adapter    = nullptr;
     std::vector<MeshDraw> meshes;
 
